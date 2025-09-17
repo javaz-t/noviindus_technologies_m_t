25f49f7bf42e3d12d249f6f68c40c7036698064c
javaz-t <147338963+javaz-t@users.noreply.github.com>
2025-09-17 13:43:34 +0530

    Invoice ui pending,PatientUpdate api in issue need ot solve,scroll down refresh pending

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,11 @@ import 'package:noviindus_technologies_m_t/data/services/register_service.dart';
 import 'package:noviindus_technologies_m_t/presentation/providers/auth_provider.dart';
 import 'package:noviindus_technologies_m_t/presentation/providers/patient_provider.dart';
 import 'package:noviindus_technologies_m_t/presentation/providers/register_provider.dart';
-import 'package:noviindus_technologies_m_t/presentation/screens/invoice/invoice_screen.dart';
-import 'package:noviindus_technologies_m_t/presentation/screens/registration/registration_screen.dart';
+import 'package:noviindus_technologies_m_t/presentation/screens/splash/splash_screen.dart';
 import 'package:provider/provider.dart';
-
 import 'data/repository/auth_repository.dart';
 import 'data/services/auth_service.dart';
 import 'data/services/patient_services.dart';
-import 'domain/entities/invoice_model.dart';
-import 'domain/entities/treatment.dart';
 
 void main() {
   runApp(
@@ -45,26 +41,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: RegistrationScreen(), //InvoiceScreen  (invoice: dummyInvoice),
+      home: SplashScreen(),
     );
   }
 }
-
-final dummyInvoice = InvoiceModel(
-  bookedOn: '16/09/2025',
-  name: 'John Doe',
-  address: '123 Main Street, Cityville',
-  whatsappNumber: '+919876543210',
-  treatmentDate: '21/02/2024',
-  treatmentTime: '11:00 AM',
-  treatments: [
-    Treatment(package: 'Hair Transplant', price: 2000, female: 2, male: 5),
-    Treatment(package: 'PRP Therapy', price: 1500, female: 2, male: 5),
-    Treatment(package: 'PRP dsfsd Therapy', price: 1500, female: 2, male: 5),
-  ],
-  totalAmount: 3500.0,
-  discountAmount: 500.0,
-  advanceAmount: 1000.0,
-  remainingAmount: 2000.0,
-  balanceAmount: 2500.0,
-);
